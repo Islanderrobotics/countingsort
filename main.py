77d8985e5935a008e24deed5f08c2455d8b1e49b
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-07-18 21:04:35 -0400

    code is done

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-def countSort(arr):
- 
-    output = [0 for i in range(len(arr))
-    count = [0 for i in range(256)]
-    ans = ["" for _ in arr]
-    for i in arr:
-        count[ord(i)] += 1
- 
-    for i in range(256):
-        count[i] += count[i-1]
- 
-    for i in range(len(arr)):
-        output[count[ord(arr[i])]-1] = arr[i]
-        count[ord(arr[i])] -= 1
- 
-    
-    for i in range(len(arr)):
-        ans[i] = output[i]
-    return ans
- 
-arr = "geeksforgeeks"
-ans = countSort(arr)
-print("Sorted character array is % s" %("".join(ans)))
+def countingSort(array):
+    size = len(array)
+    output = [0] * size
+
+    count = [0] * 10
+
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = array[i]
+        count[array[i]] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        array[i] = output[i]
+
+
+if __name__ == '__main__':
+    data = [4,1,6,2,7,8,1,5]
+    countingSort(data)
+    print("the data is sorted in ascending order")
+    print(data)
